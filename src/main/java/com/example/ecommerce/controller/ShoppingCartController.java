f3db176d39e187d6b696dcc18b45daf0aa1fcc9a
heldi.apriadi <heldi.apriadi@gli.id>
2023-01-19 15:48:59 +0700

    modify path rest controller to request mapping

--- src/main/java/com/example/ecommerce/controller/ShoppingCartController.java
+++ src/main/java/com/example/ecommerce/controller/ShoppingCartController.java
@@ -7,11 +7,13 @@ import com.example.ecommerce.response.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
-@RestController("/api/v1/shopping-cart")
+@RestController
+@RequestMapping("/api/v1/shopping-cart")
 public class ShoppingCartController {
     @Resource
     private CartOperations cartOperations;
